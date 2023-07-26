8b2adc07d5f7b7059ee69340eae237940eacbfaa
jordan <jharrison1@qa.com>
2023-07-26 10:17:42 +0100

    Imported calculator from Maven-Test

--- src/main/java/com/qa/demo/Runner.java
+++ src/main/java/com/qa/demo/Runner.java
@@ -4,5 +4,7 @@ public class Runner {
 
     public static void main(String[] args) {
         System.out.println("Maven is cool, innit?");
+
+        System.out.println(Calculator.add(12, 24));
     }
 }
